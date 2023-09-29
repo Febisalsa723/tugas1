c5678f99b2b0dd92821bcce40a97065d2da59dcb
Febisalsa723 <124450613+Febisalsa723@users.noreply.github.com>
2023-09-29 21:06:38 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama = 'febi salsabila azali'
+let nim ='20220040278'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+// aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 29;
+let b = 9;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +a+b)
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' +a-b)
